519e9070c0600086276d49ee85dddcb0b3a8dae0
Rvar <ranns95@gmail.com>
2020-10-25 18:00:31 +0100

    Updates

---
+++ precache-manifest.4e4591a88e3cabd81b9ab65e280f4435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c4f819281ebae76d6fce5c1b42e9db",
+    "url": "/tennis-game/index.html"
+  },
+  {
+    "revision": "73037226ae03833910ec",
+    "url": "/tennis-game/static/css/main.9d31794a.chunk.css"
+  },
+  {
+    "revision": "a9182d245911404c8880",
+    "url": "/tennis-game/static/js/2.3e897170.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/tennis-game/static/js/2.3e897170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73037226ae03833910ec",
+    "url": "/tennis-game/static/js/main.5f4f3699.chunk.js"
+  },
+  {
+    "revision": "78f05befababc9ae0497",
+    "url": "/tennis-game/static/js/runtime-main.3fda5099.js"
+  }
+]);
